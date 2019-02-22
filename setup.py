e421f799ea765af422ba057f75f61e9c9cabb1c5
sebastkm <s.mitusch@hotmail.com>
2019-02-22 14:22:14 +0100

    Add library

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+
+setup(name="somelibrary",
+      version="0.0.1",
+      packages=["somelibrary"]
+      )
+
---
+++ somelibrary/module.py
@@ -0,0 +1,8 @@
+def somefunc():
+    x = 0
+    for i in range(100_000):
+        x += i
+    return x
+
+
+
